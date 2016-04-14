3ba89cbb7f346442e66853ff3386e2a74478c292
twof <fabiobean2@gmail.com>
2016-04-14 11:04:55 -0700

    Added a FizzBuzz using the rem function

---
+++ fizzBuzzUsingRem.exs
@@ -0,0 +1,16 @@
+fizz_buzz = fn(0, 0, _) -> "FizzBuzz"
+(0, _, _) -> "Fizz"
+(_, 0, _) -> "Buzz"
+(_, _, c) -> c 
+end
+
+rem_fizz_buzz = fn(n) -> fizz_buzz.(rem(n, 3), rem(n, 5), n)
+end
+
+IO.puts rem_fizz_buzz.(10)
+IO.puts rem_fizz_buzz.(11)
+IO.puts rem_fizz_buzz.(12)
+IO.puts rem_fizz_buzz.(13)
+IO.puts rem_fizz_buzz.(14)
+IO.puts rem_fizz_buzz.(15)
+IO.puts rem_fizz_buzz.(16)
